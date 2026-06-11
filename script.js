d1a97b63226a0e8e9e1f717e8d586a0adeee5288
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-11 05:21:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('./cms-data.yml')
+        .then(response => response.text())
+        .then(yamlText => {
+            const data = jsyaml.load(yamlText);
+            
+            // Puts your CMS text right into the H1
+            document.getElementById('heading').innerText = data.title;
+        });
+});
