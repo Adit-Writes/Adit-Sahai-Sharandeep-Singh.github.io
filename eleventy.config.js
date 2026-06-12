7a0539fa2e36afa1440953572189bf9e00df1d05
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-11 22:06:51 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy the MainCode folder straight to the output (_site)
   eleventyConfig.addPassthroughCopy("MainCode");
+  eleventyConfig.addPassthroughCopy("files"); 
 };
