7d1276f07198f13b8e87df48139c1d315fa42201
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-12 00:16:24 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,7 @@ module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy the MainCode folder straight to the output (_site)
   eleventyConfig.addPassthroughCopy("MainCode");
   eleventyConfig.addPassthroughCopy("files");
+   return {
+    pathPrefix: "/Adit-Sahai.github.io/"
+  };
 };
