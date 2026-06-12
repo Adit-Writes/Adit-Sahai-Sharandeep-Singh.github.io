f7130b16114cde284a84e575a0a6c623d07b814d
aditsahai1901-collab <aditsahai1901@gmail.com>
2026-06-12 10:03:00 +0530

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy the MainCode folder straight to the output (_site)
   eleventyConfig.addPassthroughCopy("MainCode");
-  eleventyConfig.addPassthroughCopy("files"); 
+  eleventyConfig.addPassthroughCopy("files");
+  eleventyConfig.addPassthroughCopy("article.html");  // ← add this line
 };
