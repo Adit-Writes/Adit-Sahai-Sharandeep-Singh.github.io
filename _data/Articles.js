17021f7bf864c74fa852bee112eba24e311b9452
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-12 17:34:11 -0600

    Update Articles.js

--- _data/Articles.js
+++ _data/Articles.js
@@ -6,5 +6,18 @@ module.exports = function() {
     if (!fs.existsSync(dir)) return [];
     return fs.readdirSync(dir)
         .filter(f => f.endsWith('.json'))
-        .map(f => JSON.parse(fs.readFileSync(path.join(dir, f), 'utf8')));
+        .map(f => {
+            const article = JSON.parse(fs.readFileSync(path.join(dir, f), 'utf8'));
+            
+            // Resolve Topic reference path to actual name
+            if (article.Topic && article.Topic.endsWith('.json')) {
+                const topicPath = path.join(__dirname, '..', article.Topic);
+                if (fs.existsSync(topicPath)) {
+                    const topicData = JSON.parse(fs.readFileSync(topicPath, 'utf8'));
+                    article.Topic = topicData.name;
+                }
+            }
+            
+            return article;
+        });
 };
