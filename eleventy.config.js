50a41ab53c2ab7dca09ef6bd96b2309339b75f66
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-14 16:24:10 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("MainCode");
   eleventyConfig.addPassthroughCopy("files");
+  eleventyConfig.addPassthroughCopy("Adit");
   return {
         htmlTemplateEngine: "njk"
     };
