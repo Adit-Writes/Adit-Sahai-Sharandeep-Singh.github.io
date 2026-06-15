140b1abe20741431e431db07f4f978bddb579a77
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-15 16:31:18 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,20 +1,7 @@
-const slugify = require("slugify");
-
 module.exports = function(eleventyConfig) {
   // ONLY copy static asset folders that don't contain Eleventy templates
   eleventyConfig.addPassthroughCopy("MainCode");
   eleventyConfig.addPassthroughCopy("files");
-
-  // Register the slugify filter used throughout templates
-  // (e.g. {{ article.Title | slugify }})
-  eleventyConfig.addFilter("slugify", (str) => {
-    return slugify(String(str || ""), {
-      lower: true,
-      strict: true,   // strips special characters
-      trim: true
-    });
-  });
-
   return {
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
