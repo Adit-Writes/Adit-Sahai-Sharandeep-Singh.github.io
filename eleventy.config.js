ba7d26e97e03f498bc1ad5fd2a487c322ba4e8be
Sharandeep <166500789+Sharan041@users.noreply.github.com>
2026-06-21 02:31:35 -0600

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,9 @@
 module.exports = function(eleventyConfig) {
-  // ONLY copy static asset folders that don't contain Eleventy templates
   eleventyConfig.addPassthroughCopy("MainCode");
   eleventyConfig.addPassthroughCopy("files");
   return {
     htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk"
+    markdownTemplateEngine: "njk",
+    templateFormats: ["njk", "html", "md"]
   };
 };
